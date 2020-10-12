1d26c5bcef7d318647102e207a0e1d67cd54a1e8
Zeki Mirza <zeki.mirza@googlemail.com>
2020-10-12 09:38:21 +0100

    built the calculator layout and initial functions

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('connected')
+const display = document.querySelector('#display')
+const numbers = document.querySelectorAll('.number')
+const setDisplay = value => display.innerHTML = value
+const setEvents = () => {
+    numbers.forEach(number => number.addEventListener('click', event => addToDisplay(event.target.textContent)))
+}
+
+function addToDisplay(number){
+    let currentDisplay = display.innerHTML
+    currentDisplay == 0 ? newDisplay = number : newDisplay = currentDisplay + number
+    setDisplay(newDisplay)
+}
+
+function multiply(x,y){
+    Number(x) * Number(y)
+}
+
+window.onload = (() => {
+    setDisplay(0)
+    setEvents()
+})
